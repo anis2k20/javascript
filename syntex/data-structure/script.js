42d1657a26e6d549f8a7d2d0a95c2a1a535578e2
Anis <anis2k20@gmail.com>
2024-06-22 00:39:47 +0600

    logical assignment operator added

--- syntex/data-structure/script.js
+++ syntex/data-structure/script.js
@@ -75,3 +75,33 @@ add(5, 3, 7, 2);
 add(8, 2, 5, 3, 2, 1, 4);
 const odd = [1, 3, 5, 7, 9];
 add(...odd);
+
+// logical assignment operator
+
+const rest1 = {
+  name: "Capri",
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi",
+};
+
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
+// nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+rest2.owner = rest2.owner && "<ANONYMOUS>";
+rest1.owner = rest1.owner && "<ANONYMOUS>";
+
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1, rest2);
