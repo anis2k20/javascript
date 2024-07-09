f7f563222a444f8e09cff60624e985bde12284cc
Anis <anis2k20@gmail.com>
2024-07-09 14:17:16 +0600

    passing argument work as value vs referance

--- closer/script.js
+++ closer/script.js
@@ -13,3 +13,33 @@ function createBooking(passenger = 1, flightNum, price = passenger * 199) {
 createBooking("anis", "LH123", 200);
 createBooking(2, "B10");
 createBooking(undefined, "B10", 200);
+
+console.log("-------------Passing arguments-------------");
+
+const flight = "LH234";
+const anis = {
+  name: "Anis",
+  passport: 123456789,
+};
+
+function checkIn(flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport");
+  }
+}
+
+checkIn(flight, anis);
+console.log(flight);
+console.log(anis);
+
+const createNewpassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+createNewpassport(anis);
+checkIn(flight, anis);
