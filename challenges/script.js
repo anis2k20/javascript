2565385bd7b4ffcbea711b2f1854e408ecc54b2c
Anis <anis2k20@gmail.com>
2024-07-10 17:24:19 +0600

    Add chaining method

--- challenges/script.js
+++ challenges/script.js
@@ -259,5 +259,17 @@ function calcAverageHumanAge(ages) {
   console.log((avgHumnaAgeOfAdultDogs / adultDogs.length).toFixed(2));
 }
 
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+const calcAverageHumanAges = (ages) => {
+  ages
+    .map((age) => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter((age) => age >= 18)
+    .reduce((acc, age) => {
+      return acc + age;
+    }, 0);
+};
+
+const avg = calcAverageHumanAges([5, 2, 4, 1, 15, 8, 3]);
+
+console.log(avg);
